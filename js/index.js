08377545918d256791763d1c47aeb0a55133bbf0
seoDongGun <ehdrns96@naver.com>
2025-07-10 23:39:01 +0900

    리뉴얼 v1.2

--- js/index.js
+++ js/index.js
@@ -2,3 +2,8 @@
 // const hide = document.getElementById(subMenu)
 // document.getElementById(subMenu).style.display('flex');
 
+// 메뉴 클릭시 소메뉴 오픈
+const logitech = document.getElementById('Logitech');
+const logitechG = document.getElementById('Logitech_G');
+const software = document.getElementById('Software');
+
